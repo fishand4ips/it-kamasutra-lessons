e293aa05d7da2407ca1403fb80d973393144c7fb
fishand4ips <alekomegunov@gmail.com>
2020-09-21 16:37:40 +0300

    add store context

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import store from './redux/store-redux'
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
+import {Provider} from "./StoreContext";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state}
-                     dispatch={store.dispatch.bind(store)}
-                     store={store}
-                />
+                <Provider store={store}>
+                    <App/>
+                </Provider>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
